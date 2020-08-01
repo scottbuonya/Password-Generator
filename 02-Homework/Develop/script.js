836bd84f4dc33e4a51de74d280856729be72e3e4
Scott Buonya <scottbuonya@gmail.com>
2020-08-01 16:51:08 -0400

    made if else statements for the length of the password

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -2,41 +2,27 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+generateBtn.addEventListener("click", generatePassword);
 
-  passwordText.value = password;
 
-}
 
 function generatePassword() {
-  var passwordLength = prompt("Enter the number of characters for your password");
-  
-  var numbers = confirm("Do you want numbers?");
 
-  var lowerCase = confirm("Do you want lowercase?");
 
-  var upperCase = confirm("Do you want uppercase?");
-
-  var symbols = confirm("Do you want symbols?");
-  
-  var minimumCount = 8;
-  
   var minimumNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
-  var minimumLowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+  var minimumLowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  var minimumUpperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  var minimumUpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
   var minimumSymbols = ['!', '@', '#', '$', '%', '^', '&', '*', '?'];
-}
 
-var functionArray = {
-  getNumbers: function() {
-    return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-  },
+  var passwordLength = prompt("Enter amount of characters, must be more than 8 characters and less than 128 characters.");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("password must be 8 characters long and less than 128 characters!")
+    passwordLength = 0;
+    generatePassword()
+  } else {
+    
+  }
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
